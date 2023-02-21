2b602bfdf8e406d46689aeec35d6868934fb6989
gentomacine <ogwaluchidozie@gmail.com>
2023-02-21 01:35:17 +0100

    age added

--- models/userModel.js
+++ models/userModel.js
@@ -25,6 +25,10 @@ const userSchema = new mongoose.Schema(
             type: String,
             required: [true, "password is needed"]
         },
+        age: {
+            type: Number,
+
+        },
         created_at: {
             type: Date,
 
